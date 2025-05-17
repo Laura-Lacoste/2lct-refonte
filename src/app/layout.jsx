5dfaa8327b52c9955ddda9b810ffb4c9f0262eba
Lacoste-Laura <lacostelaura@live.fr>
2025-05-17 18:25:17 +0200

    init axeptio and GTM

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -46,6 +46,19 @@ const poppins = Poppins({
 export default function RootLayout({ children }) {
   return (
     <html lang="fr" className={poppins.variable}>
+      <head>
+  {/* Google Tag Manager */}
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-TVW9PNP7');`,
+    }}
+  />
+  {/* End Google Tag Manager */}
+</head>
       <body>
         {/*  Google Tag Manager (noscript)  */}
 <noscript><iframe
