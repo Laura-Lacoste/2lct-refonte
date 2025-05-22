209e1d001f04d0168d0dc963ad4acc771080eace
Laura-Lacoste <lacostelaura@live.fr>
2025-05-22 09:29:34 +0200

    .

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -68,8 +68,7 @@ export default function RootLayout({ children }) {
             __html: `
               <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVW9PNP7"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-          }}
+            `,}}
         />
  {/* End Google Tag Manager (noscript) */}
         <link
