973a555100ecacae1ebff998245261783cc8f40e
Lacoste-Laura <lacostelaura@live.fr>
2025-11-19 09:38:59 +0100

    deplacer gtm script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -57,17 +57,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr" className={poppins.variable}>
       <head>
-  {/* Google Tag Manager */}
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-TVW9PNP7');`,
-    }}
-  />
-  {/* End Google Tag Manager */}
   <Script
   id="schema-localbusiness-combined"
   type="application/ld+json"
@@ -221,9 +210,30 @@ export default function RootLayout({ children }) {
     ]),
   }}
 />
-
 </head>
       <body suppressHydrationWarning={true}>
+        <Script
+    id="gtm-script"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-TVW9PNP7');
+    `,
+    }}
+  />
+
+  <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-TVW9PNP7"
+      height="0"
+      width="0"
+      style={{ display: 'none', visibility: 'hidden' }}
+    ></iframe>
+  </noscript>
         <GTMNoScriptFix />
         <link
           rel="stylesheet"
